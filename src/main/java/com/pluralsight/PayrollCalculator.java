55c8fbbea119d1059d5ea7fe37d3825a4a426da7
vanessavilchis <vanessavilchis.info@gmail.com>
2025-10-07 15:34:18 -0400

    second commit

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,31 +1,65 @@
 package com.pluralsight;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class PayrollCalculator {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please Enter the name of the file employee file to process: ");
+        String inputFileName = scanner.nextLine();
+
+        System.out.print("Please Enter the name of the payroll file to create: ");
+        String outputFileName = scanner.nextLine();
         try {
             FileReader fileReader = new FileReader("src/main/resources/employees.csv");
             BufferedReader bufReader = new BufferedReader(fileReader);
             String input;
-            System.out.println("Employee ID | Name              | Gross Pay");
-            while ((input = bufReader.readLine()) != null) {
-                String[] tokens = input.split("\\|");
 
-                int employeeId = Integer.parseInt(tokens[0]);
-                String name = tokens[1];
-                double hoursWorked = Double.parseDouble(tokens[2]);
-                double payRate = Double.parseDouble(tokens[3]);
+            FileWriter fileWriter = new FileWriter("src/main/resources/" + outputFileName);
+            BufferedWriter bufWriter = new BufferedWriter(fileWriter);
 
-                Employee employee = new Employee(employeeId, name, hoursWorked, payRate);
-
-                System.out.printf("%11d | %-17s | $%,9.2f%n",
-                        employee.getEmployeeId(),
-                        employee.getName(),
-                        employee.getGrossPay());
+            boolean isJson = outputFileName.endsWith(".json");
+            if (isJson) {
+                bufWriter.write("[\n");
+            } else {
+                bufWriter.write("id|name|gross pay\n");
             }
-        } catch (IOException e) {e.printStackTrace();
+            String input;
+            Employee[] employeeList = new Employee[10];
+            int count = 0;
+
+            bufWriter.write("id|name|gross pay\n");
+
+            Employee[] employeeList = new Employee[10];
+            int count = 0;
+
+            while((input = bufReader.readLine()) != null) {
+                String[] tokens = input.split("\\|");
+                if (!tokens[0].equals("id")) { //Skipping the header row
+
+                    int id = Integer.parseInt(tokens[0]);
+                    String name = tokens[1];
+                    double hoursWorked = Double.parseDouble(tokens[2]);
+                    double payRate = Double.parseDouble(tokens[3]);
+
+                    employeeList[count] = new Employee(id, name, hoursWorked, payRate);
+
+                    bufWriter.write(String.format("%d|%s|%.2f\n",
+                            employeeList[count].getEmployeeId(),
+                            employeeList[count].getName(),
+                            employeeList[count].getGrossPay()));
+                    count++;
+                }
+            }
+
+            System.out.println(employeeList[0].getName());
+
+            bufReader.close();
+            bufWriter.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
+
     }
 }
-
